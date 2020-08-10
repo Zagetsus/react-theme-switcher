445a9e7c7a829b1ac725f628659b78164db41820
Luan Verdelho <luanverdelho642@gmail.com>
2020-08-09 22:24:14 -0300

    Criação de switch theme e um hook proprio

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+
+import userPersistedState from "./utils/usePersistedState";
+
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+
+import Header from "./components/Header";
+
+import GlobalStyles from "./styles/global";
 
 function App() {
+  const [theme, setTheme] = userPersistedState<DefaultTheme>("theme", light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyles />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
   );
 }
 
